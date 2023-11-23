7606be61d419ff3ddfebb35fece8f062c827f071
BarelyHear <bluespeller@gmail.com>
2023-11-22 20:27:05 -0700

    Statement of project intentions.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This project is entirely experimental. I don't know how video streaming works, technically speaking, but I'm still going to try and make something cool.");
+            Console.WriteLine("I would assume that as you're streaming a video, you're downloading it. I heard something once about a way of knowing your lost packets.");
+            Console.WriteLine("Because I need to simulate video streaming and loss of quality, I will first work on making the \"video\" work.");
+            Console.WriteLine("Specifically, it will be a 1920 by 1080 array of individual RGB values that are sent through a custom stream object.");
+            Console.WriteLine("At the time of this project's initialization, I have not read anthing on how to make this. I do not know how to make this. I only know how I think this");
+            Console.WriteLine("works and how I want to try and replicate it. That being said, let's try something.");
         }
     }
 }
